607ae0cfd467dbb1f3bfd0edb923e7efd9de1321
Pablo Montepagano <pablo@montepagano.com.ar>
2016-09-09 09:04:42 -0300

    Detalles de la vista de busqueda

--- wifindbar/apps/wifindbar/views.py
+++ wifindbar/apps/wifindbar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Bar
+from django.contrib import messages
 from django.contrib.gis import geos
 from django.contrib.gis import measure
 from geopy.geocoders import GoogleV3
@@ -12,14 +13,15 @@ class BaresCercaDeDireccionList(ListView):
     model = Bar
     template_name = "listar_bares.html"
     def get_queryset(self):
+        ## 0.00001 grados son aprox 1.1 metros
+        radio_en_metros = 400
+        radio_en_grados_aproximado = 0.00001 * (radio_en_metros/1.1)
         geocoder = GoogleV3()
-        direccion, latlong = geocoder.geocode(self.kwargs['direccion_busqueda'])
-        centro_de_busqueda_point = geos.Point(latlong[1], latlong[0])
-        #distancia = {'m': 400}
-        ## 0.0001 grados son aprox 10 metros
-        distancia = 0.0001 * 40
-        result = Bar.objects.filter(coordenadas__distance_lte=(centro_de_busqueda_point, distancia))
-
-        for b in result:
-            print(b)
+        location_centro_de_busqueda = geocoder.geocode(self.kwargs['direccion_busqueda'])
+        if location_centro_de_busqueda:
+            point_centro_de_busqueda = geos.Point(y=location_centro_de_busqueda.latitude, x=location_centro_de_busqueda.longitude, srid=4326)
+            result = Bar.objects.filter(coordenadas__distance_lte=(point_centro_de_busqueda, distancia))
+        else:
+            messages.warning(request, "No pudimos obtener la ubicación de la dirección ingresada.")
+            return Bar.objects.none()
         return result
