5e00438e9aef36adc660f47a9c8ec1e5691f2cdb
Pablo Montepagano <pablo@montepagano.com.ar>
2016-09-09 09:12:18 -0300

    Terminada la view que encuentra bares en un radio de X metros (aprox)

--- wifindbar/apps/wifindbar/urls.py
+++ wifindbar/apps/wifindbar/urls.py
@@ -3,5 +3,5 @@ from .views import BaresCercaDeDireccionList
 
 
 urlpatterns = [
-    url(r'(?P<direccion_busqueda>[^/]+)/$', BaresCercaDeDireccionList.as_view(), name='listar_bares_cerca_de_direccion'),
+    url(r'(?P<direccion_busqueda>[^/]+)/(?P<distancia_metros>[\d]+)/$', BaresCercaDeDireccionList.as_view(), name='listar_bares_cerca_de_direccion'),
 ]
--- wifindbar/apps/wifindbar/views.py
+++ wifindbar/apps/wifindbar/views.py
@@ -14,13 +14,13 @@ class BaresCercaDeDireccionList(ListView):
     template_name = "listar_bares.html"
     def get_queryset(self):
         ## 0.00001 grados son aprox 1.1 metros
-        radio_en_metros = 400
-        radio_en_grados_aproximado = 0.00001 * (radio_en_metros/1.1)
+        radio_en_metros = self.kwargs['distancia_metros']
+        radio_en_grados_aproximado = 0.00001 * (int(radio_en_metros)/1.1)
         geocoder = GoogleV3()
         location_centro_de_busqueda = geocoder.geocode(self.kwargs['direccion_busqueda'])
         if location_centro_de_busqueda:
             point_centro_de_busqueda = geos.Point(y=location_centro_de_busqueda.latitude, x=location_centro_de_busqueda.longitude, srid=4326)
-            result = Bar.objects.filter(coordenadas__distance_lte=(point_centro_de_busqueda, distancia))
+            result = Bar.objects.filter(coordenadas__distance_lte=(point_centro_de_busqueda, radio_en_grados_aproximado))
         else:
             messages.warning(request, "No pudimos obtener la ubicación de la dirección ingresada.")
             return Bar.objects.none()
