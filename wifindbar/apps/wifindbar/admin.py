98487a67b11dba05648829708d3e9a6df1e923c1
Pablo Montepagano <pablo@montepagano.com.ar>
2016-09-18 18:11:14 -0300

    Admin para Calificacion y Característica

--- wifindbar/apps/wifindbar/admin.py
+++ wifindbar/apps/wifindbar/admin.py
@@ -1,5 +1,8 @@
 from django.contrib.gis import admin
-from .models import Bar
+from .models import Bar, Caracteristica, Calificacion
 # Register your models here.
 
 admin.site.register(Bar, admin.OSMGeoAdmin)
+
+admin.site.register(Caracteristica)
+admin.site.register(Calificacion)
