626778401084af027a7def1d54cbda745bf19022
Pablo Montepagano <pablo@montepagano.com.ar>
2016-09-19 12:39:57 -0300

    Cambio a Google Maps en el admin

--- wifindbar/apps/wifindbar/admin.py
+++ wifindbar/apps/wifindbar/admin.py
@@ -1,8 +1,11 @@
-from django.contrib.gis import admin
+from django.contrib import admin
 from .models import Bar, Caracteristica, Calificacion
+from .forms import BarModelForm
 # Register your models here.
 
-admin.site.register(Bar, admin.OSMGeoAdmin)
+class BarAdmin(admin.ModelAdmin):
+    form = BarModelForm
 
+admin.site.register(Bar, BarAdmin)
 admin.site.register(Caracteristica)
 admin.site.register(Calificacion)
